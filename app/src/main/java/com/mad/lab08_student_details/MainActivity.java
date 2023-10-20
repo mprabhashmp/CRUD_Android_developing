f8dfea3a2d621bb99d7da6a12cc2074f202f133a
mprabhashmp <120329245+mprabhashmp@users.noreply.github.com>
2023-10-20 14:51:50 +0530

    main activity class

--- app/src/main/java/com/mad/lab08_student_details/MainActivity.java
+++ app/src/main/java/com/mad/lab08_student_details/MainActivity.java
@@ -2,13 +2,29 @@ package com.mad.lab08_student_details;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnInsert;
+    Button btnView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnInsert = findViewById(R.id.btnInsert);
+        btnView = findViewById(R.id.btnview);
+
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),AddActivity.class));
+
+            }
+        });
     }
 }
